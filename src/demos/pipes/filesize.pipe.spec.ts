034b0e267917b6bff9d7ef8120a5b364eecd2e75
AlexandreJareck <alexandrejareck@gmail.com>
2024-07-03 13:53:49 -0300

    test: teste comportamental do file size pipe

--- src/demos/pipes/filesize.pipe.spec.ts
+++ src/demos/pipes/filesize.pipe.spec.ts
@@ -1,3 +1,5 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FileSizePipe } from './fileSize.pipe';
 
 describe('FileSizePipe', () => {
@@ -11,6 +13,43 @@ describe('FileSizePipe', () => {
 
     it('Deve converter bytes para GB', () => {
       expect(pipe.transform(1342177280)).toBe('1.25 GB');
+    });
+  });
+
+  describe('Teste comportamental do pipe', () => {
+    @Component({
+      template: ` Size: {{ size | filesize }} `,
+    })
+    class TestComponent {
+      size = 123456789;
+    }
+
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [FileSizePipe, TestComponent],
+      });
+
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      el = fixture.nativeElement;
+    });
+
+    it('Deve converter bytes para MB', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74 MB');
+      component.size = 1029281;
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 0.98 MB');
+    });
+
+    it('Deve converter bytes para GB', () => {
+      component.size = 1342177280;
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 1.25 GB');
     });
   });
 });
